c34d8cf982dfeef653299fc389f0b4552259ef9d
Lawrence-afi <Okonlawrence00@gmail.com>
2025-11-11 11:41:37 -1100

    added few config for the routes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
+  base: "/",
+  server: {
+    historyApiFallback: true, // ensures reloads on routes work in dev
+  },
+});
